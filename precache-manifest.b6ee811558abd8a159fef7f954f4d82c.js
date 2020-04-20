b5330449c5475ee74768dc9374e9ed38a6d32172
apple <apple@MacBook.attlocal.net>
2020-04-20 10:48:55 -0500

    Updates

---
+++ precache-manifest.b6ee811558abd8a159fef7f954f4d82c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d7bafe881151d1965bc496cc2a198b",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "18a4ae7330d38149bf57",
+    "url": "/project-4-assessment/static/css/main.7d7bcc0f.chunk.css"
+  },
+  {
+    "revision": "910bf80e1b28f638c5a0",
+    "url": "/project-4-assessment/static/js/2.19cdf6eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.19cdf6eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a4ae7330d38149bf57",
+    "url": "/project-4-assessment/static/js/main.d6db4d7e.chunk.js"
+  },
+  {
+    "revision": "669c81cd0dab0e9f774f",
+    "url": "/project-4-assessment/static/js/runtime-main.b4ab89fe.js"
+  }
+]);
